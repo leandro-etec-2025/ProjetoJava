3f7dd113842f836bc24c30ba2c35896582c2eeb7
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-05-30 10:45:57 -0300

    Adiconado mais métodos

--- ProjetoJava/src/ClasseAlunos.java
+++ ProjetoJava/src/ClasseAlunos.java
@@ -3,4 +3,29 @@ public class ClasseAlunos {
 	String nomeAt; // Abaixo da class é atributo é não var
 	float notaAt; 
 	String cursoAt;
+	
+	// Método para mostrar nome
+	public void mostrarNomeMet() {
+		System.out.println(this.nomeAt);
+	}
+
+	public void mostrarNotaMet() {
+		System.out.println(this.notaAt);
+	}
+	
+	public void mostrarCursoMet() {
+		System.out.println(this.cursoAt);
+	}
+	
+	public void receberNomeMet(String nomePar) {
+		this.nomeAt = nomePar;
+	}
+
+	public void receberNotaMet(float notaPar) {
+		this.notaAt = notaPar;
+	}
+
+	public void receberCursoMet(String cursoPar) {
+		this.cursoAt = cursoPar;
+	}
 }
--- ProjetoJava/src/ClassePrincipal.java
+++ ProjetoJava/src/ClassePrincipal.java
@@ -9,14 +9,23 @@ public class ClassePrincipal {
 		ClasseAlunos abraaoObj = new ClasseAlunos(); // Objeto 2
 		
 		// Funciona, mas está errado
-		mikaelObj.nomeAt = "Mikael";
-		mikaelObj.cursoAt = "Informática";
-		mikaelObj.notaAt = 8.5F;
+		// mikaelObj.nomeAt = "Mikael";
+		// mikaelObj.cursoAt = "Informática";
+		// mikaelObj.notaAt = 8.5F;
+		
+		mikaelObj.receberNomeMet("Mikael");
+		mikaelObj.receberCursoMet("Informática");
+		//mikaelObj.mostrarNotaMet(8.5F);
+		
 		
 		// Funciona, mas está errado
-		System.out.println(mikaelObj.nomeAt); // sysout CTRL + SPACE
-		System.out.println(mikaelObj.cursoAt);
-		System.out.println(mikaelObj.notaAt);
+		//System.out.println(mikaelObj.nomeAt); // sysout CTRL + SPACE
+		//System.out.println(mikaelObj.cursoAt);
+		//System.out.println(mikaelObj.notaAt);
+		mikaelObj.mostrarNomeMet();
+		mikaelObj.mostrarCursoMet();
+		mikaelObj.mostrarNotaMet();
+
 		
 	}
 	
